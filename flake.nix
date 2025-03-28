bac78e7daf133acf523c79505a8643bd4e8dd5e9
TeakMirror113 <59761933+Ansh-Sonkusare@users.noreply.github.com>
2025-03-29 02:29:11 +0530

    wip: Cloudflare

--- flake.nix
+++ flake.nix
@@ -3,34 +3,41 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          system = system;
-          overlays = [
-            (final: prev: rec {
-              kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
-                plugins = with prev.kubernetes-helmPlugins; [
-                  helm-diff
-                  helm-secrets
-                  helm-s3
-                ];
-              };
-            })
-          ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "helmfile devShell";
-          nativeBuildInputs = with pkgs; [
-            bashInteractive
-          ];
-          buildInputs = with pkgs; [
-            kubernetes-helm-wrapped
-            helmfile-wrapped
-          ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        system = system;
+        overlays = [
+          (final: prev: rec {
+            kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
+              plugins = with prev.kubernetes-helmPlugins; [
+                helm-diff
+                helm-secrets
+                helm-s3
+              ];
+            };
+          })
+        ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "helmfile devShell";
+        nativeBuildInputs = with pkgs; [
+          bashInteractive
+        ];
+        buildInputs = with pkgs; [
+          kubernetes-helm-wrapped
+          helmfile-wrapped
+        ];
+        shellHook = ''
+          export SHELL=$(which zsh)
+          export NVIM_APPNAME=nvim-chad
+          exec zsh
+        '';
+      };
+    });
 }
